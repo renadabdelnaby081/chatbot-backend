9aabc43bda913aa3e832e23900ff83ac41cd9845
renadabdelnaby <renadabdelnaby081@gmail.com>
2025-12-13 02:16:26 -0800

    done

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../model/User.js"); // ← هنا التعديل الصحيح
+const User = require("../models/User.js"); // ← هنا التعديل الصحيح
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
