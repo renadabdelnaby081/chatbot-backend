f82ec206b845162ec600818faa1181ccd79546b7
renadabdelnaby <renadabdelnaby081@gmail.com>
2025-12-13 02:05:42 -0800

    done2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const authRoutes = require("./routs/auth");
+const authRoutes = require("./routes/auth");
 const rateLimit = require("express-rate-limit");
 
 const app = express();
